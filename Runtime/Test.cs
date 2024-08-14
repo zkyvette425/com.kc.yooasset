edcfb832d9d7c1091129fb760aca6b8aa882866c
Kzzzzzzz <450228418@qq.com>
2024-08-14 15:03:36 +0800

    一个测试版本

---
+++ Runtime/Test.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+using YooAsset;
+
+namespace KC
+{
+    public class Test : MonoBehaviour
+    {
+        private void Start()
+        {
+            YooAssets.Initialize();
+        }
+    }
+}
+
