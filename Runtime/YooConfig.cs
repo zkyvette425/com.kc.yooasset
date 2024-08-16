4c85c5a63addea43137f44f58e4d41fca2db46a4
Kzzzzzzz <450228418@qq.com>
2024-08-16 18:28:23 +0800

    添加热更代码

---
+++ Runtime/YooConfig.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+using YooAsset;
+
+namespace KC
+{
+    [CreateAssetMenu(menuName = "YooAsset/YooConfig",fileName = "YooConfig",order = 0)]
+    public class YooConfig : ScriptableObject
+    {
+        public EPlayMode playMode;
+
+        public string[] cdn;
+
+        public int useCdnIndex;
+
+        public string CDN => cdn[useCdnIndex];
+    }
+}
+
